513a4d444eae5c65c31ea8c20ae67d1523b35964
Aditya Parikh <102518677+androbrine@users.noreply.github.com>
2023-02-21 16:43:15 +0530

    ..

--- EXPS/exp 3/MergeSort.java
+++ EXPS/exp 3/MergeSort.java
@@ -58,7 +58,7 @@ public class MergeSort {
             System.out.print(array[i] + " ");
         }
 
-        System.out.println("Before sorting:");
+        System.out.println("\nBefore sorting:");
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " "); 
         }
