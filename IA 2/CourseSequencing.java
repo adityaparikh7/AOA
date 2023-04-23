c35117ed624ba93248cfe4aeb3e687a69fe50ec7
adityaparikh7 <asparikh03@gmail.com>
2023-04-23 15:35:45 +0530

    first commit

---
+++ IA 2/CourseSequencing.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class CourseSequencing {
+
+    public static void main(String[] args) {
+        // Define the list of courses and their prerequisites
+        Map<String, List<String>> courseList = new HashMap<>();
+        courseList.put("CS101", Arrays.asList("MATH101"));
+        courseList.put("MATH101", new ArrayList<>());
+        courseList.put("CS201", Arrays.asList("CS101"));
+        courseList.put("CS301", Arrays.asList("CS201"));
+
+        // Calculate the optimal course sequence
+        List<String> optimalSequence = calculateOptimalSequence(courseList);
+
+        // Print the optimal course sequence
+        System.out.println("Optimal course sequence: " + optimalSequence);
+    }
+
+    public static List<String> calculateOptimalSequence(Map<String, List<String>> courseList) {
+        // Initialize the memoization table
+        Map<String, List<String>> memoTable = new HashMap<>();
+
+        // Calculate the optimal sequence for each course
+        for (String course : courseList.keySet()) {
+            calculateOptimalSequenceHelper(course, courseList, memoTable);
+        }
+
+        // Find the maximum sequence length
+        int maxLength = 0;
+        for (List<String> sequence : memoTable.values()) {
+            if (sequence.size() > maxLength) {
+                maxLength = sequence.size();
+            }
+        }
+
+        // Find the optimal sequence with the maximum length
+        List<String> optimalSequence = new ArrayList<>();
+        for (List<String> sequence : memoTable.values()) {
+            if (sequence.size() == maxLength && isSequenceValid(sequence, courseList)) {
+                optimalSequence = sequence;
+                break;
+            }
+        }
+
+        return optimalSequence;
+    }
+
+    private static List<String> calculateOptimalSequenceHelper(String course, Map<String, List<String>> courseList, Map<String, List<String>> memoTable) {
+        // Check if the optimal sequence for this course has already been calculated
+        if (memoTable.containsKey(course)) {
+            return memoTable.get(course);
+        }
+
+        // Calculate the optimal sequence for each prerequisite course
+        List<String> optimalSequence = new ArrayList<>();
+        for (String prerequisite : courseList.get(course)) {
+            List<String> prerequisiteSequence = calculateOptimalSequenceHelper(prerequisite, courseList, memoTable);
+            if (prerequisiteSequence.size() > optimalSequence.size()) {
+                optimalSequence = prerequisiteSequence;
+            }
+        }
+
+        // Add this course to the optimal sequence
+        optimalSequence.add(course);
+
+        // Store the optimal sequence in the memoization table
+        memoTable.put(course, optimalSequence);
+
+        return optimalSequence;
+    }
+
+    private static boolean isSequenceValid(List<String> sequence, Map<String, List<String>> courseList) {
+        // Check that all prerequisites are satisfied
+        for (int i = 1; i < sequence.size(); i++) {
+            String course = sequence.get(i);
+            List<String> prerequisites = courseList.get(course);
+            for (String prerequisite : prerequisites) {
+                if (!sequence.contains(prerequisite)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
