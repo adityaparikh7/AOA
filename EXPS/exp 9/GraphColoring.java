8b192606c513a86fcd089a298baa6fdb061710a4
adityaparikh7 <asparikh03@gmail.com>
2023-04-25 17:01:18 +0530

    update

---
+++ EXPS/exp 9/GraphColoring.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class GraphColoring {
+    private int numOfVertices;
+    private int numOfColors;
+    private int[] colors;
+    private int[][] graph;
+
+    public GraphColoring(int numOfVertices, int numOfColors) {
+        this.numOfVertices = numOfVertices;
+        this.numOfColors = numOfColors;
+        this.colors = new int[numOfVertices];
+        this.graph = new int[numOfVertices][numOfVertices];
+        generateGraph();
+    }
+
+    private void generateGraph() {
+        Random random = new Random();
+        for (int i = 0; i < numOfVertices; i++) {
+            for (int j = i + 1; j < numOfVertices; j++) {
+                int randInt = random.nextInt(2);
+                graph[i][j] = randInt;
+                graph[j][i] = randInt;
+            }
+        }
+    }
+
+    public void solve() {
+        if (colorGraph(0)) {
+            printColors();
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private boolean colorGraph(int nodeIndex) {
+        if (nodeIndex == numOfVertices) {
+            return true;
+        }
+
+        for (int colorIndex = 1; colorIndex <= numOfColors; colorIndex++) {
+            if (isColorValid(nodeIndex, colorIndex)) {
+                colors[nodeIndex] = colorIndex;
+
+                if (colorGraph(nodeIndex + 1)) {
+                    return true;
+                }
+
+                colors[nodeIndex] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isColorValid(int nodeIndex, int colorIndex) {
+        for (int i = 0; i < numOfVertices; i++) {
+            if (graph[nodeIndex][i] == 1 && colorIndex == colors[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void printColors() {
+        System.out.println("Solution exists: ");
+        for (int i = 0; i < numOfVertices; i++) {
+            System.out.println("Node " + (i + 1) + " has color index: " + colors[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int numOfVertices = 5;
+        int numOfColors = 3;
+        GraphColoring graphColoring = new GraphColoring(numOfVertices, numOfColors);
+        graphColoring.solve();
+    }
+}
--- EXPS/exp 9/graphcolor.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class graphcolor {
-    public static void main(String[] args) {
-        
-    }
-}
