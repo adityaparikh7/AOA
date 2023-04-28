a6fb8b1004e4b64473f22de4ae8647ca667a0c8f
adityaparikh7 <asparikh03@gmail.com>
2023-04-28 14:18:10 +0530

    first commit

---
+++ EXPS/exp 10/lcs.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class lcs {
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter the first string:");
+    // String s1 = sc.nextLine();
+    // System.out.println("Enter the second string:");
+    // String s2 = sc.nextLine();
+    // int m = s1.length();
+    // int n = s2.length();
+    // int[][] lcs = new int[m + 1][n + 1];
+    // for (int i = 0; i <= m; ++i) {
+    // lcs[i][0] = 0;
+    // }
+    // for (int j = 0; j <= n; ++j) {
+    // lcs[0][j] = 0;
+    // }
+    // for (int i = 1; i <= m; ++i) {
+    // char c1 = s1.charAt(i - 1);
+    // for (int j = 1; j <= n; ++j) {
+    // char c2 = s2.charAt(j - 1);
+    // if (c1 == c2) {
+    // lcs[i][j] = lcs[i - 1][j - 1] + 1;
+    // } else {
+    // int a = lcs[i - 1][j];
+    // int b = lcs[i][j - 1];
+    // lcs[i][j] = Math.max(a, b);
+    // }
+    // }
+    // }
+    // System.out.println("The length of the longest common subsequence is " +
+    // lcs[m][n]);
+    // sc.close();
+
+    public static String findLCS(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String s1 = sc.nextLine();
+        System.out.println("Enter the second string:");
+        String s2 = sc.nextLine();
+        System.out.println("The length of the longest common subsequence is " + findLCS(s1, s2));
+        sc.close();
+    }
+
+}
